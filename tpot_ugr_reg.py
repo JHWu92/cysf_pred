7820b1ae52493e44a68a8a108471bc3e168e481d
JHWu92 <jhwu92@gmail.com>
2017-09-03 15:32:54 -0400

    add metrics to tpot experiment

--- tpot_ugr_reg.py
+++ tpot_ugr_reg.py
@@ -2,14 +2,25 @@ from tpot import TPOTRegressor
 from sklearn.model_selection import train_test_split
 import numpy as np
 import pandas as pd
+from sklearn.metrics import mean_squared_error, f1_score, accuracy_score
 
 y_name = 'ugr_all'
-X = pd.read_csv('data/%s_x_nototal_2014.csv' % y_name, index_col=0)
+X = pd.read_csv('data/%s_x_total_2014.csv' % y_name, index_col=0)
 y = pd.read_csv('data/y_%s.csv' % y_name, index_col=0)
 # X = X.loc[y.index]
 X_train, X_test, y_train, y_test = train_test_split(X.astype(np.float64), y.ugr.astype(np.float64), test_size=0.2, random_state=0)
 
-tpot = TPOTRegressor(verbosity=2, n_jobs=4, random_state=0)
+tpot = TPOTRegressor(verbosity=2, n_jobs=4, random_state=0, generations=100)
 tpot.fit(X_train, y_train)
 print(tpot.score(X_test, y_test))
-tpot.export('tpot_pipeline_reg_%s.py' % y_name)
+tpot.export('tpot_pipeline_reg_total2014_%s.py' % y_name)
+
+pred = tpot.predict(X_test).round()
+y_test = y_test.round()
+
+mse_train = mean_squared_error(y_test, pred)
+acc_train = accuracy_score(y_test, pred)
+f1_weighted = f1_score(y_test, pred, average='weighted')
+f1_macro = f1_score(y_test, pred, average='macro')
+
+print('mse_train =', mse_train, 'acc_train =', acc_train,  'f1_weighted =', f1_weighted, 'f1_macro =', f1_macro)
