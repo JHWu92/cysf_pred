73b6249d309b472c19cd02a967dec1c354e32258
JHWu92 <jhwu92@gmail.com>
2017-09-04 15:27:46 -0400

    Improved caching ML ready features

--- ml_ready_features.py
+++ ml_ready_features.py
@@ -3,6 +3,7 @@
 from src.ftr_aggregate import load_features
 import pandas as pd
 from src.constants import dir_data
+import pickle
 
 totals = ['NO_TOTAL', 'TOTAL']
 years_choices = [('~2014', (2014, 2015, 2016, 2017)), ('~2016', (2016, 2017)), ]
@@ -13,3 +14,5 @@ for total_or_not in totals:
         ftrs, cols_by_type = load_features(None, how=total_or_not, years=years, pair_with_y=False)
         features[(total_or_not, year_type)] = (ftrs, cols_by_type)
         ftrs.to_csv('data/x_%s_%s.csv' %(total_or_not, year_type))
+        with open('data/x_%s_%s_cols_by_type.pkl' %(total_or_not, year_type), 'wb') as f:
+            pickle.dump(cols_by_type, f)
